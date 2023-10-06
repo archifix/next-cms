c76a425893a27403e0e49d678d7811fd1599f76c
Илья <i.dontsov61rus@yandex.ru>
2023-10-06 18:03:32 +0300

    Update

---
+++ front/src/components/axios/axios.tsx
@@ -0,0 +1,28 @@
+// import axios from 'axios'
+
+//     const API_TOKEN = process.env.API_TOKEN
+//   const API_URL = process.env.API_URL
+
+// axios.get('http://cms:1337/api/news').then(response => {
+//   console.log(response.data);
+// });
+
+
+// News.getInitialProps = async ctx => {
+//     try {
+//       const res = await axios.get('http://cms:1337/api/news');
+//       const news = res.data;
+//       return { news };
+//     } catch (error) {
+//       return { error };
+//     }
+//   };
+//   News.getInitialProps = async ctx => {
+//     try {
+//       const res = await axios.get('http://cms:1337/api/news');
+//       const news = res.data;
+//       return { news };
+//     } catch (error) {
+//       return { error };
+//     }
+//   };
